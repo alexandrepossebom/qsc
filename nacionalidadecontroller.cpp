e0aabae6877dd939817149a70dff4cb24e9d518b
Alexandre Possebom <alexandre@localhost.(none)>
2009-09-22 19:33:54 -0300

    - insert ceps.

--- nacionalidadecontroller.cpp
+++ nacionalidadecontroller.cpp
@@ -7,34 +7,59 @@ NacionalidadeController::NacionalidadeController()
 
 QString NacionalidadeController::getError()
 {
-    qDebug() << "ola";
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
-    db.setDatabaseName("ceps");
+    db.setDatabaseName("loja");
     db.setUserName("root");
     db.setPassword("");
 
     if (!db.open()) {
         error = db.lastError().text();
         qDebug() << error;
-        return false;
     }
-
-
     QSqlQuery query;
+    query.prepare("select cidade,bairro,tp_logradouro,logradouro,cep from pr where cidade = :cidade");
+    query.bindValue(":cidade", "sao jose dos pinhais");
 
-    query.prepare("select logradouro,bairro,cep from pr where cidade = 'São José dos Pinhais' order by cep");
-
-      if (!query.exec())
+    if (!query.exec())
     {
         error = query.lastError().text();
         qDebug() << error;
-
     }
 
-   while (query.next()) {
-        qDebug() << query.value(0).toString();
-        qDebug() << query.value(1).toString();
+    QString endereco;
+    QString bairro;
+    QString cep;
+    QString cidade;
+    int i = 0;
+    while (query.next()) {
+        i++;
+        cidade = query.value(0).toString();
+        bairro = query.value(1).toString();
+        endereco = query.value(2).toString().append(" ").append( query.value(3).toString() );
+        cep = query.value(4).toString().replace("-","");
+        qDebug() << cidade << bairro << endereco << cep;
+        QSqlQuery queryCidade;
+        queryCidade.prepare("insert into cidade (estado_uf,nome) VALUES ('PR',:nome)");
+        queryCidade.bindValue(":nome", cidade);
+        if(!queryCidade.exec()){
+            qDebug() << queryCidade.lastError().text();
+            continue;
+        }
+        QSqlQuery queryEndereco;
+        queryEndereco.prepare("insert into endereco (cidade_id,nome) VALUES (:id,:nome)");
+        queryEndereco.bindValue(":nome", endereco);
+        queryEndereco.bindValue(":id", queryCidade.lastInsertId().toInt());
+        if(!queryEndereco.exec())
+            continue;
+        QSqlQuery queryCep;
+        queryCep.prepare("insert into cep (endereco_id,cep) VALUES (:id,:cep)");
+        queryCep.bindValue(":id", queryEndereco.lastInsertId().toInt());
+        queryCep.bindValue(":cep", cep.toInt());
+        if(!queryCep.exec())
+            continue;
+        qDebug() << i;
     }
 
     return error;
